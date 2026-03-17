b165ff0f08c2f30f6120c156527a5459ac19b87f
jusiann <adilefe257@gmail.com>
2026-03-17 15:55:22 +0300

    added Products and Categories Routes

--- api/app.js
+++ api/app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import "dotenv/config";
 
-import AUTH_ROUTES from "./src/routes/auth.routes.js";
+import AUTH_ROUTES from "./src/routes/auth.router.js";
+import PRODUCTS_ROUTES from "./src/routes/products.router.js";
+import CATEGORIES_ROUTES from "./src/routes/categories.router.js";
 import { connectDB, runSeed } from "./src/lib/db/database.js";
 
 const PORT = process.env.PORT || 3000;
@@ -10,45 +12,47 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-    const start = Date.now();
-    const originalSend = res.send;
-    res.send = function (data) {
-        const duration = Date.now() - start;
-        const time = new Date().toLocaleTimeString("tr-TR", { hour12: false });
-        console.log(
-            `[LOG - ${time}] ${req.method} ${req.url} ${res.statusCode} ${duration}ms`,
-        );
-        return originalSend.call(this, data);
-    };
-    next();
+  const start = Date.now();
+  const originalSend = res.send;
+  res.send = function (data) {
+    const duration = Date.now() - start;
+    const time = new Date().toLocaleTimeString("tr-TR", { hour12: false });
+    console.log(
+      `[LOG - ${time}] ${req.method} ${req.url} ${res.statusCode} ${duration}ms`,
+    );
+    return originalSend.call(this, data);
+  };
+  next();
 });
 
-app.use('/api/auth', AUTH_ROUTES);
+app.use("/api/auth", AUTH_ROUTES);
+app.use("/api/products", PRODUCTS_ROUTES);
+app.use("/api/categories", CATEGORIES_ROUTES);
 
 app.get("/health", (req, res) => {
-    const time = new Date().toLocaleTimeString("tr-TR", { hour12: false });
-    res.status(200).json({
-        success: true,
-        message: `[SERVER - ${time}] System is healthy`,
-        timestamp: new Date().toISOString(),
-        uptime: process.uptime(),
-    });
+  const time = new Date().toLocaleTimeString("tr-TR", { hour12: false });
+  res.status(200).json({
+    success: true,
+    message: `[SERVER - ${time}] System is healthy`,
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
 });
 
 app.use((req, res) => {
-    res.status(404).json({ success: false, message: "Route not found" });
+  res.status(404).json({ success: false, message: "Route not found" });
 });
 
 app.use((err, req, res, next) => {
-    console.error("Global error handler:", err);
-    res
-        .status(err.statusCode || 500)
-        .json({ success: false, message: err.message || "Internal server error" });
+  console.error("Global error handler:", err);
+  res
+    .status(err.statusCode || 500)
+    .json({ success: false, message: err.message || "Internal server error" });
 });
 
 app.listen(PORT, async () => {
-    const time = new Date().toLocaleTimeString("tr-TR", { hour12: false });
-    console.log(`[SERVER - ${time}] Started on port ${PORT}`);
-    await connectDB();
-    await runSeed();
+  const time = new Date().toLocaleTimeString("tr-TR", { hour12: false });
+  console.log(`[SERVER - ${time}] Started on port ${PORT}`);
+  await connectDB();
+  await runSeed();
 });
