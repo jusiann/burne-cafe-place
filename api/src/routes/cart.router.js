cc38c029bd5544b693135b7e6bc750ff6c71bdfe
jusiann <adilefe257@gmail.com>
2026-03-21 00:51:24 +0300

    update cart routing logic

--- api/src/routes/cart.router.js
+++ api/src/routes/cart.router.js
@@ -1,21 +1,21 @@
 import express from 'express';
 import { verifyToken } from '../middlewares/auth.js';
-import { 
+import {
     validateCoupon,
-    getCart, 
-    addItemToCart, 
-    updateCartItem, 
-    removeCartItem, 
-    clearCart 
+    getCart,
+    addItemToCart,
+    updateCartItem,
+    removeCartItem,
+    clearCart
 } from '../controllers/cart.controller.js';
 
 const router = express.Router();
 
 router.get('/', verifyToken, getCart);
-router.post('/coupons/validate', verifyToken, validateCoupon);
-router.post('/items', verifyToken, addItemToCart);
 router.put('/items/:itemId', verifyToken, updateCartItem);
 router.delete('/items/:itemId', verifyToken, removeCartItem);
 router.delete('/', verifyToken, clearCart);
+router.post('/items', verifyToken, addItemToCart);
+router.post('/coupons/validate', verifyToken, validateCoupon);
 
 export default router;
